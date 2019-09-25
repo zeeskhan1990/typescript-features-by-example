a2aea09988ec88969229248f0876a1bc7d207e17
Zeeshan Khan <zeeskhan1990@gmail.com>
2019-09-25 19:10:08 +0530

    Multi sort

--- sample-2/src/index.ts
+++ sample-2/src/index.ts
@@ -3,11 +3,13 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
+const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -15,6 +17,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
