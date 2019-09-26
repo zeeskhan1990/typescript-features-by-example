6ee611c956434e84788aacd9f5879848025bf4e1
Zeeshan Khan <zeeskhan1990@gmail.com>
2019-09-26 11:51:22 +0530

    Multiple reader implementation

--- sample-3/src/index.ts
+++ sample-3/src/index.ts
@@ -1,25 +1,18 @@
-import fs from 'fs';
+import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map(row => {
-    return row.split(',');
-  });
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
 
-console.log(matches);
-
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D '
-}
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
