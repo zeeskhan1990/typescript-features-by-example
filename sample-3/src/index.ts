893622eff44e3f1384826d2eb65186d9c53677cb
Zeeshan Khan <zeeskhan1990@gmail.com>
2019-09-26 10:16:54 +0530

    Ongoing

---
+++ sample-3/src/index.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map(row => {
+    return row.split(',');
+  });
+
+console.log(matches);
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D '
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
